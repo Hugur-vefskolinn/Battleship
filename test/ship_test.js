d5a2ea0806145399563d97ed46c225979350fc66
Anna Sóley <annasoley09@gmail.com>
2018-03-08 14:34:40 +0000

    added coordinates all test pass

--- test/ship_test.js
+++ test/ship_test.js
@@ -16,4 +16,15 @@ describe("checkForShip", function () {
   };
     expect(checkForShip(player, [9,9])).to.be.false;
   });
+  it(" should correctly report no ship at a given players coordinate", function () {
+
+    player = {
+       ships: [
+        {
+          locations:[[0, 0]]
+        }
+      ]
+    };
+      expect(checkForShip(player, [0,0])).to.be.true;
+    });
 });
