ece28ff15280266c7b299d16733448d0916d360c
Mikael Karlsson <mikaelk7@gmail.com>
2018-03-08 13:53:47 +0000

    added ship testing and ship methods

---
+++ test/ship_test.js
@@ -0,0 +1,11 @@
+
+//first Commit = this test plus adding the game logic folder og ship_methods.js file
+
+var expect = require("chai").expect;
+
+describe("checkForShip", function () {
+  var checkForShip = require("../game_logic/ship_methods").checkForShip;
+  it(" should correctly report no ship at a given players coordinate", function () {
+    
+  });
+});
