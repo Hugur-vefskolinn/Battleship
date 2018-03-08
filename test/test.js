3ff14d22854565400b70d97a4a2b12df72174690
Mikael Karlsson <mikaelk7@gmail.com>
2018-03-08 10:59:44 +0000

    first test

---
+++ test/test.js
@@ -0,0 +1,12 @@
+
+//////////////
+//first commit 
+//////////////
+var expect = require("chai").expect;
+//test suite
+describe("Mocha", function () {
+  //Test spec (unit test)
+  it("should run our tests using npm", function () {
+    expect(true).to.be.ok;
+  });
+});
